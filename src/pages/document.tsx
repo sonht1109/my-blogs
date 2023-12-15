f8c0b928782cdd240099227fe4f8e551c4b97ed6
sonht1109 <yourgitemail@example.com>
2023-12-15 11:32:49 +0700

    update

---
+++ src/pages/document.tsx
@@ -0,0 +1,34 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  const meta = {
+    title: "Blogs created by sonht",
+    description: "Collection of software development knowledge.",
+    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+  };
+
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Official_R_deep" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
