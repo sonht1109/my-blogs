d441a000f59594a6d9bb8b46dc06190c625808c3
sonht1109 <yourgitemail@example.com>
2023-12-17 14:22:38 +0700

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,12 +14,12 @@ export default function Document() {
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content="/large-preview.png" />
-        <meta name="twitter:card" content="summary_large_image`" />
+        <meta property="og:image" content="https://blogs.sonht.io.vn/large-preview.png" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@_sonht" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content="/large-preview.png" />
+        <meta name="twitter:image" content="https://blogs.sonht.io.vn/large-preview.png" />
 
         <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
       </Head>
