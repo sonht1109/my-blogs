a5747b61f2384b368165221095d9d63e32f60574
sonht1109 <yourgitemail@example.com>
2023-12-17 15:39:44 +0700

    update keywords

--- theme.config.js
+++ theme.config.js
@@ -92,7 +92,7 @@ const config = {
       {meta.description && (
         <meta name="description" content={meta.description} />
       )}
-      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.keywords && <meta name="keywords" content={meta.keywords} />}
       {meta.author && <meta name="author" content={meta.author} />}
     </>
   ),
