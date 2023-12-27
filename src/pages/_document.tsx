9fdf4ec22f8a911b3ca85311a538623978a86b8b
sonht1109 <yourgitemail@example.com>
2023-12-28 00:14:37 +0700

    add: page about

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Blogs created by sonht",
+    title: "Blogs created by sonht1109",
     description: "Collection of software development knowledge.",
   };
 
@@ -16,7 +16,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content="https://blogs.sonht.io.vn/large-preview.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@_sonht" />
+        <meta name="twitter:site" content="@sonht1109" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content="https://blogs.sonht.io.vn/large-preview.png" />
