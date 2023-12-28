44aba57f06a2c31ca692a37898b053a8df8efaeb
sonht1109 <yourgitemail@example.com>
2023-12-28 13:59:39 +0700

    chore: remove speedinsight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import "../globals.css";
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function MyApp({ Component, pageProps }: any) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
     </>
   );
 }
