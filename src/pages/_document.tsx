046c2c49ddf31a15a8f50ac7d78854c475ce81c9
sonht1109 <yourgitemail@example.com>
2023-12-28 13:43:37 +0700

    update: metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,23 +2,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Blogs created by sonht1109",
+    title: "Blog created by sonht1109",
     description: "Collection of software development knowledge.",
   };
 
   return (
     <Html lang="en">
       <Head>
-        <meta name="robots" content="follow, index" />
+        <meta name="robots" content="follow,index" />
         <meta name="description" content={meta.description} />
         <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
         <meta property="og:image" content="https://blogs.sonht.io.vn/large-preview.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@sonht1109" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content="https://blogs.sonht.io.vn/large-preview.png" />
 
         <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
