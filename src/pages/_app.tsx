f386fbc8ae6b834a39f84e843724675f3c1be471
sonht1109 <yourgitemail@example.com>
2023-12-28 13:45:15 +0700

    chore: add speedinsight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function MyApp({ Component, pageProps }: any) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
