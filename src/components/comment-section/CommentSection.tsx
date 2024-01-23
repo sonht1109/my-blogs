a36eb63611527fde74a74156be6287c81c5c4cea
sonht1109 <yourgitemail@example.com>
2024-01-23 22:31:29 +0700

    chore: add robot

--- src/components/comment-section/CommentSection.tsx
+++ src/components/comment-section/CommentSection.tsx
@@ -1,28 +1,29 @@
 import React, { useEffect, useRef } from "react";
 
 const CommentSection = () => {
-  const ref = useRef<HTMLDivElement | null>(null);
+  // const ref = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const script = document.createElement("script");
+  // useEffect(() => {
+  //   const script = document.createElement("script");
 
-    const config = {
-      src: "https://utteranc.es/client.js",
-      repo: "sonht1109/my-blogs",
-      "issue-term": "pathname",
-      crossOrigin: "anonymous",
-      theme: "github-light",
-      defer: "true",
-    };
+  //   const config = {
+  //     src: "https://utteranc.es/client.js",
+  //     repo: "sonht1109/my-blogs",
+  //     "issue-term": "pathname",
+  //     crossOrigin: "anonymous",
+  //     theme: "github-light",
+  //     defer: "true",
+  //   };
 
-    Object.entries(config).forEach(([key, value]) => {
-      script.setAttribute(key, value);
-    });
+  //   Object.entries(config).forEach(([key, value]) => {
+  //     script.setAttribute(key, value);
+  //   });
 
-    ref.current?.append(script);
-  }, []);
+  //   ref.current?.append(script);
+  // }, []);
 
-  return <div ref={ref} />;
+  // return <div ref={ref} />;
+  return <></>
 };
 
 export default CommentSection;
