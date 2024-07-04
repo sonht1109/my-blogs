4333aac6fd11c18ce92270feb3c4c78cc118fbb5
sonht1109 <sonht1109@gmail.com>
2024-07-04 04:34:01 +0000

    chore: add image preview

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../globals.css";
+import 'react-photo-view/dist/react-photo-view.css';
+
 import { Analytics } from '@vercel/analytics/react';
+import { PhotoProvider } from "react-photo-view";
 
 export default function MyApp({ Component, pageProps }: any) {
   return (
-    <>
+    <PhotoProvider>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </PhotoProvider>
   );
 }
