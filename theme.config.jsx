bdba12869365e7f6ac6b93e597e4a3fc08105c7c
sonht1109 <sonht1109@gmail.com>
2024-09-14 16:45:57 +0000

    update footer

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,4 +1,4 @@
-import CommentSection from '@/components/comment-section'
+import CommentSection from "@/components/comment-section";
 
 const config = {
   footer: (
@@ -7,6 +7,32 @@ const config = {
       <div className="flex justify-between items-center">
         <p>© 2023 - {new Date().getFullYear()} by sonht1109</p>
         <div className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <a
+            target="_blank"
+            href="https://sonht.io.vn"
+            className="ml-2 text-gray-500"
+            rel="noreferrer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-world"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+              <path d="M3.6 9h16.8" />
+              <path d="M3.6 15h16.8" />
+              <path d="M11.5 3a17 17 0 0 0 0 18" />
+              <path d="M12.5 3a17 17 0 0 1 0 18" />
+            </svg>
+          </a>
           <a
             target="_blank"
             href="https://github.com/sonht1109"
@@ -21,25 +47,6 @@ const config = {
               fill="currentColor"
             >
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-            </svg>
-          </a>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/sn.hngth/"
-            className="ml-2 text-gray-500"
-            rel="noreferrer"
-          >
-            <svg
-              fill="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-            >
-              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
             </svg>
           </a>
           <a
@@ -98,9 +105,15 @@ const config = {
       {meta.author && <meta name="author" content={meta.author} />}
       {meta.title && <meta property="og:title" content={meta.title} />}
       {meta.title && <meta property="twitter:title" content={meta.title} />}
-      {meta.description && <meta property="og:description" content={meta.description} />}
-      {meta.description && <meta property="twitter:description" content={meta.description} />}
-      {meta.description && <meta name="description" content={meta.description} />}
+      {meta.description && (
+        <meta property="og:description" content={meta.description} />
+      )}
+      {meta.description && (
+        <meta property="twitter:description" content={meta.description} />
+      )}
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
     </>
   ),
   readMore: "Read More →",
